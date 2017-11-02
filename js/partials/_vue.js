aeed4bdb5dbdd10a7d2cbd7313c5a3baf49286af
Lemon <AhoyLemon@users.noreply.github.com>
2017-11-02 16:31:20 -0500

    Hearts & Squares.

--- js/partials/_vue.js
+++ js/partials/_vue.js
@@ -3,25 +3,47 @@ var app = new Vue({
   el: '#app',
   data: {
     men: dumbMen,
-    filePath: 'img/men/',
-    raindrops: []
+    masks: [ 'heart-1', 'heart-2', 'heart-3', 'heart-4' ],
+    filePath: 'img/men/squares/',
+    raindrops: [],
+    recentMen: []
   },
   methods: {
     newRaindrop: function() {
       var self = this;
       var r = Math.floor(Math.random()*self.men.length);
+      
+      if (self.recentMen.includes(self.men[r].file)) {
+        self.newRaindrop();
+      } else {
+        self.recentMen.push(self.men[r].file);
+        if (self.recentMen.length > 12) {
+          self.recentMen.shift();
+        }
+      }
+      
+      var dropClass = self.masks[Math.floor(Math.random()*self.masks.length)];
 
       var sizeDiff = ((Math.floor(Math.random() * 100) + 1) / 100);
+      //alert(sizeDiff);
       
-      var s = 0.3 + (sizeDiff * 0.7);
-      var tX = (-100 + (Math.random() * 200)) + '%';
-      var aD = 7 - (3 * sizeDiff) + 's';
-      var rot = (-4 + (Math.random() * 8)) + 'deg';
+      var minSize = 0.05;
+      var maxSize = 1.05;
+      
+      var s = minSize + (sizeDiff * (maxSize - minSize));
+      var tX = (-120 + (Math.random() * 240)) + '%';
+      
+      // Fall speed
+      var minSpeed = 2;
+      var maxSpeed = 18;
+      var aD = maxSpeed - ((maxSpeed - minSpeed) * sizeDiff) + 's';
+      
+      var rot = (-16 + (Math.random() * 32)) + 'deg';
       var shd = Math.floor(30 * sizeDiff) + 'px'
       
       var drop = {
         file: self.men[r].file,
-        //scale: t,
+        class: dropClass,
         seconds: {
           animationDuration: aD,
           zIndex: (sizeDiff * 1000)
@@ -31,10 +53,13 @@ var app = new Vue({
           boxShadow: '0 0 '+shd+' rgba(0,0,0,0.5)'
         }
       };
+      
       self.raindrops.push(drop);
+      
       if (self.raindrops.length > 20) {
-        //self.raindrops.shift();
+        $('#Raindrops .drop:first-child').remove();
       }
+      
     }
   },
   mounted: function () {
@@ -44,7 +69,7 @@ var app = new Vue({
     function doSomething() {}
 
     (function loop() {
-      var rand = Math.round(Math.random() * (100  - 200)) + 200;
+      var rand = Math.round(Math.random() * (1600  - 600)) + 600;
       setTimeout(function() {
         //alert('A');
         self.newRaindrop();
